58c63fc92c3c02f24ff8874a6a943ca03a93cf37
Anamooz <145793103+Anamooz@users.noreply.github.com>
2025-01-11 19:37:41 -0800

    fixed css wip

---
+++ my-app/main.js
@@ -0,0 +1,18 @@
+import './style.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: [1000, 1000],
+    zoom: 2.5
+  })
+});
+

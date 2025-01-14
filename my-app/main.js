c8fd9b8cdbfb759eb4ce7c2bf5968f536999ae4a
Anamooz <145793103+Anamooz@users.noreply.github.com>
2025-01-13 18:30:16 -0800

    finished

--- my-app/main.js
+++ my-app/main.js
@@ -5,25 +5,63 @@ import OSM from 'ol/source/OSM';
 import Point from 'ol/geom/Point.js';
 import sourceLayer from 'ol/source/Vector';
 import VectorLayer from 'ol/layer/Vector';
+import {Circle, Fill, Stroke, Style} from 'ol/style.js';
 
 const home = new Feature({ 
-  geometry: new Point([-120.426636, 34.948991])
+  geometry: new Point([-120.426636, 34.948991]),
 });
 
 const family = new Feature({ 
-  geometry: new Point([-122.275085, 37.847748])
+  geometry: new Point([-122.275085, 37.847748]),
 });
 
 const friends = new Feature({ 
-  geometry: new Point([-120.657349, 35.277016])
+  geometry: new Point([-120.657349, 35.277016]),
 });
 
-const vectorSource = new sourceLayer({
-  features: [home, family, friends],
+const vectorSourceHome = new sourceLayer({
+  features: [home]
 });
 
-const markedVectorLayer = new VectorLayer({
-  source: vectorSource,
+const vectorSourceFamily = new sourceLayer({
+  features: [family]
+});
+
+const vectorSourceFriends = new sourceLayer({
+  features: [friends]
+});
+
+const markedVectorLayerHome = new VectorLayer({
+  source: vectorSourceHome,
+  style: new Style({
+    image: new Circle({
+      radius: 5,
+      fill: new Fill({color: 'red'}),
+      stroke: new Stroke({color: 'black', width: 1}),
+    }),
+  }),
+});
+
+const markedVectorLayerFamily = new VectorLayer({
+  source: vectorSourceFamily,
+  style: new Style({
+    image: new Circle({
+      radius: 30,
+      fill: new Fill({color: 'blue'}),
+      stroke: new Stroke({color: 'black', width: 1}),
+    }),
+  }),
+});
+
+const markedVectorLayerFriends = new VectorLayer({
+  source: vectorSourceFriends,
+  style: new Style({
+    image: new Circle({
+      radius: 10,
+      fill: new Fill({color: 'green'}),
+      stroke: new Stroke({color: 'black', width: 1}),
+    }),
+  }),
 });
 
 const map = new Map({
@@ -40,5 +78,8 @@ const map = new Map({
   })
 });
 
-map.addLayer(markedVectorLayer);
+map.addLayer(markedVectorLayerHome);
+map.addLayer(markedVectorLayerFamily);
+map.addLayer(markedVectorLayerFriends);
+ 
 
